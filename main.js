d0e2d0b20e85c98eb84f8459fc90c4c01626c6b0
worldmaker <worldmaker18349276@gmail.com>
2018-06-01 07:37:32 +0800

    abort Jupyter shortcut

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
 define(['base/js/namespace'], function(Jupyter) {
     function load_ipython_extension() {
-        document.getElementById('site').style.scrollBehavior = 'smooth';
-
         Jupyter.notebook.config.loaded.then(function() {
             var step = Jupyter.notebook.config.data.littlescroll_step;
+            var step_edit = 20;
 
             var littledown = Jupyter.keyboard_manager.actions.register({
                 help       : 'Scroll down a little',
                 help_index : 'zz',
-                handler    : function() { document.getElementById('site').scrollBy(0,  step); }
+                handler    : function() { document.getElementById('site').scrollBy({left:0,  top: step, behavior:'smooth'}); }
             }, 'scroll-down-a-little', 'little_scroll');
             var littleup = Jupyter.keyboard_manager.actions.register({
                 help       : 'Scroll up a little',
                 help_index : 'zz',
-                handler    : function() { document.getElementById('site').scrollBy(0, -step); }
+                handler    : function() { document.getElementById('site').scrollBy({left:0,  top:-step, behavior:'smooth'}); }
             }, 'scroll-up-a-little', 'little_scroll');
 
-            Jupyter.keyboard_manager.command_shortcuts.add_shortcuts({
-                'ctrl-down' : littledown,
-                'ctrl-up'   : littleup
-            });
+            document.addEventListener('keydown', function(e){
+                if (e.ctrlKey && e.keyCode == 40) {
+                    if (document.body.classList.contains('edit_mode'))
+                        document.getElementById('site').scrollBy({left:0,  top: step_edit, behavior:'auto'});
+                    else
+                        document.getElementById('site').scrollBy({left:0,  top: step, behavior:'smooth'});
+                    e.preventDefault();
+                    e.stopPropagation();
+                    return false;
+                } else if (e.ctrlKey && e.keyCode == 38) {
+                    if (document.body.classList.contains('edit_mode'))
+                        document.getElementById('site').scrollBy({left:0,  top:-step_edit, behavior:'auto'});
+                    else
+                        document.getElementById('site').scrollBy({left:0,  top:-step, behavior:'smooth'});
+                    e.preventDefault();
+                    e.stopPropagation();
+                    return false;
+                }
+            }, true);
+
+//             Jupyter.keyboard_manager.command_shortcuts.add_shortcuts({
+//                 'ctrl-down' : littledown,
+//                 'ctrl-up'   : littleup
+//             });
+//             Jupyter.keyboard_manager.edit_shortcuts.add_shortcuts({
+//                 'ctrl-down' : littledown,
+//                 'ctrl-up'   : littleup
+//             });
         });
     }
 
