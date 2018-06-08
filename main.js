2f421610852ec34894abb9f715e4d2eafc666d40
worldmaker <worldmaker18349276@gmail.com>
2018-06-08 19:40:57 +0800

    rewrite by scroll_manager

--- main.js
+++ main.js
@@ -1,48 +1,39 @@
-define(['base/js/namespace'], function(Jupyter) {
+define(['jquery', 'base/js/namespace'], function($, Jupyter) {
     function load_ipython_extension() {
         Jupyter.notebook.config.loaded.then(function() {
-            var step = Jupyter.notebook.config.data.littlescroll_step;
-            var step_edit = 20;
+            var settings = $.extend({step:0.3, step_in_edit:20}, Jupyter.notebook.config.data.LittleScroll);
 
             var littledown = Jupyter.keyboard_manager.actions.register({
                 help       : 'Scroll down a little',
                 help_index : 'zz',
-                handler    : function() { document.getElementById('site').scrollBy({left:0,  top: step, behavior:'smooth'}); }
+                handler    : function() { Jupyter.notebook.scroll_manager.scroll( settings.step); }
             }, 'scroll-down-a-little', 'little_scroll');
             var littleup = Jupyter.keyboard_manager.actions.register({
                 help       : 'Scroll up a little',
                 help_index : 'zz',
-                handler    : function() { document.getElementById('site').scrollBy({left:0,  top:-step, behavior:'smooth'}); }
+                handler    : function() { Jupyter.notebook.scroll_manager.scroll(-settings.step); }
             }, 'scroll-up-a-little', 'little_scroll');
 
             document.addEventListener('keydown', function(e){
-                if (e.ctrlKey && e.keyCode == 40) {
-                    if (document.body.classList.contains('edit_mode'))
-                        document.getElementById('site').scrollBy({left:0,  top: step_edit, behavior:'auto'});
-                    else
-                        document.getElementById('site').scrollBy({left:0,  top: step, behavior:'smooth'});
-                    e.preventDefault();
-                    e.stopPropagation();
-                    return false;
-                } else if (e.ctrlKey && e.keyCode == 38) {
-                    if (document.body.classList.contains('edit_mode'))
-                        document.getElementById('site').scrollBy({left:0,  top:-step_edit, behavior:'auto'});
-                    else
-                        document.getElementById('site').scrollBy({left:0,  top:-step, behavior:'smooth'});
-                    e.preventDefault();
-                    e.stopPropagation();
-                    return false;
+                if (document.body.classList.contains('edit_mode')) {
+                    if (e.ctrlKey && e.keyCode == 40) {
+                        document.getElementById('site').scrollBy(0,  settings.step_in_edit);
+                        e.preventDefault();
+                        e.stopPropagation();
+                        return false;
+                    } else if (e.ctrlKey && e.keyCode == 38) {
+                        document.getElementById('site').scrollBy(0, -settings.step_in_edit);
+                        e.preventDefault();
+                        e.stopPropagation();
+                        return false;
+                    }
                 }
             }, true);
-
-//             Jupyter.keyboard_manager.command_shortcuts.add_shortcuts({
-//                 'ctrl-down' : littledown,
-//                 'ctrl-up'   : littleup
-//             });
-//             Jupyter.keyboard_manager.edit_shortcuts.add_shortcuts({
-//                 'ctrl-down' : littledown,
-//                 'ctrl-up'   : littleup
-//             });
+            
+            Jupyter.keyboard_manager.command_shortcuts.add_shortcuts({
+                'ctrl-down' : littledown,
+                'ctrl-up'   : littleup
+            });
         });
     }
 
